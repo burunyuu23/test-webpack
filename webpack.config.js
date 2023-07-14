3646a2e6e8c96676bdd03869eae803d18c8ef45b
danilkk <ezzfvkoko@gmail.com>
2023-07-14 23:28:36 +0300

    favicon + image export + compress

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,7 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         clean: true,
         filename: '[name].[contenthash].js',
+        assetModuleFilename: 'assets/[hash][ext]'
     },
     plugins: [new HtmlWebpackPlugin({
         template: path.resolve(__dirname, 'src', 'index.html')
@@ -55,6 +56,34 @@ module.exports = {
                 // Compiles Sass to CSS
                 "sass-loader",
               ],
+            },
+            {
+                test: /\.(jpe?g|png|webp|gif|svg)$/i,
+                use: [
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            mozjpeg: {
+                              progressive: true,
+                            },
+                            // optipng.enabled: false will disable optipng
+                            optipng: {
+                              enabled: false,
+                            },
+                            pngquant: {
+                              quality: [0.65, 0.90],
+                              speed: 4
+                            },
+                            gifsicle: {
+                              interlaced: false,
+                            },
+                            // the webp option will enable WEBP
+                            webp: {
+                              quality: 75
+                            }
+                          }
+                    }
+                ]
             },
             {
                 test: /\.woff2?$/i,
